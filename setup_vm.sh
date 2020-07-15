ff364db6a3245062e0c5c4ed1d44a055b865d636
tlecoeuv <tlecoeuv@student.42.fr>
2020-07-15 17:15:12 +0200

    metallb fonctionne

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,10 +1,11 @@
 minikube start --vm-driver=docker
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
 
 eval $(minikube docker-env)
 
-docker build -t my_nginx srcs/nginx
-docker build -t my_mysql srcs/mysql
-docker build -t my_wordpress srcs/wordpress
-docker build -t my_phpmyadmin srcs/phpmyadmin
+docker build -t my_nginx srcs/containers/nginx
+docker build -t my_mysql srcs/containers/mysql
+docker build -t my_wordpress srcs/containers/wordpress
+docker build -t my_phpmyadmin srcs/containers/phpmyadmin
 
-kubectl apply -k ./srcs/
+kubectl apply -k ./srcs/deployment
