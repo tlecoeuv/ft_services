12ae21855a5a44d5f9a27a3d85c897519ecfb7ca
tlecoeuv <tlecoeuv@student.42.fr>
2020-09-01 17:45:26 +0200

    bons ports

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,4 +1,6 @@
 minikube start --vm-driver=docker
+
+MINIKUBE_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
 
 eval $(minikube docker-env)
@@ -7,6 +9,10 @@ docker build -t my_nginx srcs/containers/nginx
 docker build -t my_mysql srcs/containers/mysql
 docker build -t my_wordpress srcs/containers/wordpress
 docker build -t my_phpmyadmin srcs/containers/phpmyadmin
+docker build -t my_influxdb srcs/containers/influxdb
+docker build -t my_telegraf srcs/containers/telegraf
+docker build -t my_grafana srcs/containers/grafana
 
 kubectl apply -k ./srcs/deployment
+
 kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
