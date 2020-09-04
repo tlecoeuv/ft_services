95b76616d0e9ab37df9a551ae0a7d270105a7bef
tlecoeuv <tlecoeuv@student.42.fr>
2020-09-04 12:09:03 +0200

    avant test du nouveau setup

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,6 +1,26 @@
 minikube start --vm-driver=docker
 
 MINIKUBE_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
+WORDPRESS_IP=172.17.0.7
+METALLB_IP_RANGE=172.17.0.3-172.17.0.13
+FTPS_IP=172.17.0.3
+
+
+
+cp srcs/containers/wordpress/srcs/wordpress_model.sql srcs/containers/wordpress/srcs/wordpress.sql
+sed -i "s/WORDPRESS_IP/$WORDPRESS_IP/g" srcs/containers/wordpress/srcs/wordpress.sql
+
+cp srcs/deployment/metallb-config_model.yaml srcs/deployment/metallb-config.yaml
+sed -i "s/METALLB_IP_RANGE/$METALLB_IP_RANGE/g" srcs/deployment/metallb-config.yaml
+
+cp srcs/containers/telegraf/srcs/telegraf_model.conf srcs/containers/telegraf/srcs/telegraf.conf
+sed -i "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/containers/telegraf/srcs/telegraf.conf
+
+cp srcs/containers/ftps/srcs/start_model.sh srcs/containers/ftps/srcs/start.sh
+sed -i "s/FTPS_IP/$FTPS_IP/g" srcs/containers/ftps/srcs/start.sh
+
+
+
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
 
 eval $(minikube docker-env)
@@ -12,6 +32,7 @@ docker build -t my_phpmyadmin srcs/containers/phpmyadmin
 docker build -t my_influxdb srcs/containers/influxdb
 docker build -t my_telegraf srcs/containers/telegraf
 docker build -t my_grafana srcs/containers/grafana
+docker build -t my_ftps srcs/containers/ftps
 
 kubectl apply -k ./srcs/deployment
 
